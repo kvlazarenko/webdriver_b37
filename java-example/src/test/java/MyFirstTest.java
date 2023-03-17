2530473d31424e4e06b2ca69f25b40e3615ace7e
kvlazarenko <kv.lazarenko@gmail.com>
2023-03-17 14:09:43 +0300

    Тест "Запуск браузера"

---
+++ java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @BeforeEach
+  public void start() {
+
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.navigate().to("http://www.google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("btnK")).click();
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+  @AfterEach
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
